d75c69f23cd866dfbecef27e8b3b026b3260ad1a
towhid7667 <towhidulhq@gmail.com>
2022-08-31 16:16:04 +0600

    search results complete

--- foods.js
+++ foods.js
@@ -1,5 +1,5 @@
 let loadMeals = (search) => {
-  const url = 'https://www.themealdb.com/api/json/v1/1/search.php?s=${search}'
+  const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${search}`;
   console.log(url);
   fetch(url)
     .then(res => res.json())
@@ -7,33 +7,25 @@ let loadMeals = (search) => {
 
 }
 
-const displayMeals = (meals) => {
-  let mealsElement = document.getElementById("meals");
+const displayMeals = meals => {
+  let mealsElement = document.getElementById("meals-list");
   mealsElement.innerHTML = '';
 
-  // meals.forEach(meal => {
-  //   let cardBody = document.createElement('div');
-  //   cardBody.classList.add('col');
-  //   cardBody.innerHTML = `
-  //       <div class="card h-100">
-  //     <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
-  //     <div class="card-body">
-  //       <h5 class="card-title">${meal.strMeal}</h5>
-  //       <p class="card-text">${meals.strInstructions.slice(0, 200)}</p>
-  //     </div>
-  //       `;
-  //   mealsElement.appendChild(cardBody);
-
-
-  // });
-
   meals.forEach(meal => {
-    console.log(meal);
+    let cardBody = document.createElement('div');
+    cardBody.classList.add('col');
+    cardBody.innerHTML = `
+        <div class="card h-100">
+      <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">${meal.strMeal}</h5>
+        <p class="card-text">${meal.strInstructions.slice(0, 200)}</p>
+      </div>
+        `;
+    mealsElement.appendChild(cardBody);
+
 
   });
-
-
-
 }
 
 
@@ -48,4 +40,4 @@ const searchDisplay = () => {
 
 
 
-loadMeals();
+loadMeals('');
