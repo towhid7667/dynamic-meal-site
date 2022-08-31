27e36ff5ca157dec275df131464ad58a5749c6e8
towhid7667 <towhidulhq@gmail.com>
2022-08-31 13:14:03 +0600

    testing-01

--- foods.js
+++ foods.js
@@ -1,33 +1,51 @@
 let loadMeals = (search) => {
-    const url = 'https://www.themealdb.com/api/json/v1/1/search.php?f=${search}'
-    fetch(url)
-        .then(res => res.json())
-        .then(data => displayMeals(data.meals))
+  const url = 'https://www.themealdb.com/api/json/v1/1/search.php?s=${search}'
+  console.log(url);
+  fetch(url)
+    .then(res => res.json())
+    .then(data => displayMeals(data.meals))
 
 }
 
 const displayMeals = (meals) => {
-    let mealsElement = document.getElementById("meals");
+  let mealsElement = document.getElementById("meals");
+  mealsElement.innerHTML = '';
 
-    meals.forEach(meal => {
-        let cardBody = document.createElement('div');
-        cardBody.classList.add('col');
-        cardBody.innerHTML = `
-        <div class="card h-100">
-      <img src="${meals[0].strMealThumb}" class="card-img-top" alt="...">
-      <div class="card-body">
-        <h5 class="card-title">${meals[0].strMeal}</h5>
-        <p class="card-text">${meals[0].strInstructions.slice(0, 200)}</p>
-      </div>
-        `
-        mealsElement.appendChild(cardBody);
+  // meals.forEach(meal => {
+  //   let cardBody = document.createElement('div');
+  //   cardBody.classList.add('col');
+  //   cardBody.innerHTML = `
+  //       <div class="card h-100">
+  //     <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
+  //     <div class="card-body">
+  //       <h5 class="card-title">${meal.strMeal}</h5>
+  //       <p class="card-text">${meals.strInstructions.slice(0, 200)}</p>
+  //     </div>
+  //       `;
+  //   mealsElement.appendChild(cardBody);
 
 
-    });
+  // });
+
+  meals.forEach(meal => {
+    console.log(meal);
+
+  });
 
 
 
 }
+
+
+const searchDisplay = () => {
+  const mealFindElement = document.getElementById('mealField');
+  const mealFind = mealFindElement.value;
+  loadMeals(mealFind);
+  mealFindElement.value = '';
+}
+
+
+
 
 
 loadMeals();
